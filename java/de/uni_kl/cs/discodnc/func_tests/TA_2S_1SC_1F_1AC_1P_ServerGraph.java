e9620cb9799e01bd26d3947de582057f93155b77
sbondorf <steffen@s-bondorf.de>
2018-12-18 19:49:07 +0100

    Plural package name according to this advice: singular when the classes in the package are classification and plural when they are implementations

---
+++ java/de/uni_kl/cs/discodnc/func_tests/TA_2S_1SC_1F_1AC_1P_ServerGraph.java
@@ -0,0 +1,99 @@
+/*
+ * This file is part of the Disco Deterministic Network Calculator.
+ *
+ * Copyright (C) 2013 - 2018 Steffen Bondorf
+ * Copyright (C) 2017+ The DiscoDNC contributors
+ *
+ * Distributed Computer Systems (DISCO) Lab
+ * University of Kaiserslautern, Germany
+ *
+ * http://discodnc.cs.uni-kl.de
+ *
+ *
+ * The Disco Deterministic Network Calculator (DiscoDNC) is free software;
+ * you can redistribute it and/or modify it under the terms of the 
+ * GNU Lesser General Public License as published by the Free Software Foundation; 
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+package de.uni_kl.cs.discodnc.func_tests;
+
+import de.uni_kl.cs.discodnc.curves.ArrivalCurve;
+import de.uni_kl.cs.discodnc.curves.Curve;
+import de.uni_kl.cs.discodnc.curves.ServiceCurve;
+import de.uni_kl.cs.discodnc.network.server_graph.Flow;
+import de.uni_kl.cs.discodnc.network.server_graph.Server;
+import de.uni_kl.cs.discodnc.network.server_graph.ServerGraph;
+import de.uni_kl.cs.discodnc.network.server_graph.ServerGraphFactory;
+
+public class TA_2S_1SC_1F_1AC_1P_ServerGraph implements ServerGraphFactory {
+	private final int sc_R = 10;
+	private final int sc_T = 10;
+	private final int ac_r = 5;
+	private final int ac_b = 25;
+	
+	private Server s0, s1;
+	
+	private ServiceCurve service_curve = Curve.getFactory().createRateLatency(sc_R, sc_T);
+	private ArrivalCurve arrival_curve = Curve.getFactory().createTokenBucket(ac_r, ac_b);
+	
+	private ServerGraph server_graph;
+
+	public TA_2S_1SC_1F_1AC_1P_ServerGraph() {
+		server_graph = createServerGraph();
+	}
+
+	public ServerGraph getServerGraph() {
+		return server_graph;
+	}
+
+	public ServerGraph createServerGraph() {
+		server_graph = new ServerGraph();
+
+		s0 = server_graph.addServer(service_curve);
+		s0.useMaxSC(false);
+		s0.useMaxScRate(false);
+
+		s1 = server_graph.addServer(service_curve);
+		s1.useMaxSC(false);
+		s1.useMaxScRate(false);
+
+		try {
+			server_graph.addTurn(s0, s1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+
+		try {
+			server_graph.addFlow("f0", arrival_curve, s0, s1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+
+		return server_graph;
+	}
+
+	public void reinitializeCurves() {
+		service_curve = Curve.getFactory().createRateLatency(sc_R, sc_T);
+		for (Server server : server_graph.getServers()) {
+			server.setServiceCurve(service_curve);
+		}
+
+		arrival_curve = Curve.getFactory().createTokenBucket(ac_r, ac_b);
+		for (Flow flow : server_graph.getFlows()) {
+			flow.setArrivalCurve(arrival_curve);
+		}
+	}
+}
